fe85ec946ce6097de76a1739071893bb23ddc3b4
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-06-06 23:04:25 +0700

    first commit

---
+++ openCV/build/intermediates/cxx/Debug/6k4v325q/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\minht\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Thuchanh\\FaceDetect\\openCV\\.cxx\\Debug\\6k4v325q\\x86" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/Debug/6k4v325q/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\minht\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Thuchanh\\FaceDetect\\openCV\\.cxx\\Debug\\6k4v325q\\x86_64" ^
+  opencv_jni_shared
